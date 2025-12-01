68fd52847ce3542d03578efe840c03182abf66ff
ayscd <ayeskacsta@gmail.com>
2025-12-01 07:00:09 -0300

    setting up the search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from "./logo.svg"
+import Dictionary from "./Dictionary";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="logo img-fluid" alt="logo" />
-      </header>
+      <div className="container"> 
+        <header>
+          <div className="header">
+            <span><img src={logo} className="logo img-fluid" alt="logo" /></span>
+            <span><h1>LEXICON DICTIONARY</h1></span>
+          </div>
+        </header>
+        <main className="main">
+          <Dictionary />
+        </main>
+        <footer className="footer">This app was developed by <a href="https://www.linkedin.com/in/ayeskaasc" className="author-link" target="_blank" rel="noreferrer">Ayeska</a>. You can see it on <a href="https://github.com/ayscd/react-dictionary-app" className="git-link" target="_blank" rel="noreferrer">Github</a>.</footer>
+      </div>
     </div>
   );
 }
