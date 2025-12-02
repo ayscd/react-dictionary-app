c5f0ea144ee688d2f5533698647e841e16045c08
ayscd <ayeskacsta@gmail.com>
2025-12-01 21:02:30 -0300

    displaying word phonetics

--- src/Results.js
+++ src/Results.js
@@ -9,7 +9,10 @@ export default function Results(props) {
     if (props.results) {
         return(
             <div className="Results">
-                <h2>{props.results.word}</h2>            
+                <header>
+                    <h2>{props.results.word}</h2>
+                    <span>{props.results.phonetic}</span>           
+                </header>
                 <div>{props.results.meanings.map(function(meaning, index) {
                     return(
                         <div key={index}>
